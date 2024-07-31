9805afc69953e50f0cc1569103e9810a4d99f00f
kooqooo <koogmlcks@gmail.com>
2024-07-31 21:21:02 +0900

    feat: 데이터를 LangChain의 Document로 구성하기
    #7

--- data/vectordb_manager.py
+++ data/vectordb_manager.py
@@ -24,7 +24,28 @@ def read_json(path: str | Path) -> List[Dict[str, str | bool | dict[str, int]]]:
     with open(path, "r") as f:
         return json.load(f)
 
+def make_documents(items: List[Dict]) -> List[Document]:
+    documents = []
+    for item in items:
+        document = Document(
+            page_content=item["description"],
+            metadata={key: value for key, value in item.items() if key != "description"}
+        )
+        documents.append(document)
+    return documents
 
 if __name__ == "__main__":
-    path = Path("./fake_data.json")
-    data = read_json(path)
+    import os
+    
+    # 경로 지정
+    path = os.path.join(os.path.dirname(__file__), "fake_data.json")
+
+    items = read_json(path) # JSON 파일 읽기
+    documents = make_documents(items)  # Document 객체 생성
+
+    # 테스트
+    assert type(documents[0].page_content) == str, "설명은 str이여야 합니다."
+    assert type(documents[0].metadata) == dict, "metadata는 dict여야 합니다."
+    assert type(documents[0].metadata["pricing"]["L"]) == int, "가격은 int여야 합니다."
+    assert type(documents[0].metadata["is_popular"]) == bool, "인기 메뉴 여부는 bool이여야 합니다."
+    print("테스트 통과!")
