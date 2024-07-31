ed0cf9d47016a40a8ea97cd597363b8fe6e4cd9a
kooqooo <koogmlcks@gmail.com>
2024-07-31 21:37:17 +0900

    feat: 오디오 입력으로 whisper STT

--- stt/whisper.py
+++ stt/whisper.py
@@ -8,7 +8,7 @@ load_dotenv(override=True)
 client = OpenAI()
 
 
-def whisper_transcribe(audio_file_path: str | Path, prompt: str | None) -> str:
+def whisper_transcribe_from_file(audio_file_path: str | Path, prompt: str | None) -> str:
     with open(audio_file_path, "rb") as audio_file:
         transcript = client.audio.transcriptions.create(
             language="ko",
@@ -18,9 +18,18 @@ def whisper_transcribe(audio_file_path: str | Path, prompt: str | None) -> str:
         )
     return transcript.text
 
+def whisper_transcribe_from_audio(audio: bytes, prompt: str | None) -> str:
+    transcript = client.audio.transcriptions.create(
+        language="ko",
+        model="whisper-1",
+        file=audio,
+        prompt=prompt if prompt else None
+    )
+    return transcript.text
+
 
 if __name__ == "__main__":
     audio_file_path = "./음성메모.m4a"
     audio_file_path = Path("./음성메모.m4a")
-    transcript = whisper_transcribe(audio_file_path, "구희찬")
+    transcript = whisper_transcribe_from_file(audio_file_path, "구희찬")
     print(transcript)
